003bd060cc19d432e891910dad65bd1a7c18f4fb
솔 <sol@sol-ui-Macmini.local>
2026-07-13 16:36:09 +0900

    Upgrade Voyage travel workflow

---
+++ sw.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'voyage-travel-kit-v1';
+const APP_SHELL = [
+  './',
+  './index.html',
+  'https://unpkg.com/react@18.3.1/umd/react.development.js',
+  'https://unpkg.com/react-dom@18.3.1/umd/react-dom.development.js',
+  'https://unpkg.com/@babel/standalone@7.29.0/babel.min.js',
+  'https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/variable/pretendardvariable.min.css'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil((async () => {
+    const cache = await caches.open(CACHE_NAME);
+    await Promise.all(APP_SHELL.map(async url => {
+      try {
+        const response = await fetch(url, {mode: 'no-cors'});
+        await cache.put(url, response);
+      } catch (_) {}
+    }));
+    await self.skipWaiting();
+  })());
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil((async () => {
+    const names = await caches.keys();
+    await Promise.all(names.filter(name => name !== CACHE_NAME).map(name => caches.delete(name)));
+    await self.clients.claim();
+  })());
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith((async () => {
+    const cached = await caches.match(event.request);
+    if (cached) return cached;
+    try {
+      const response = await fetch(event.request);
+      const cache = await caches.open(CACHE_NAME);
+      cache.put(event.request, response.clone());
+      return response;
+    } catch (_) {
+      return caches.match('./index.html');
+    }
+  })());
+});
